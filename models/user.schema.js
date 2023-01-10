8a1afa744c9f8f1c1c910a57cc7958d390c666a2
anupam-k <anupamkrishnan4@gmail.com>
2023-01-10 10:44:26 +0530

    Hooks for User Schema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,35 +1,45 @@
 import mongoose from "mongoose";
-import AuthRoles from '../utils/authRoles';
+import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcrypt";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Name is required"],
-            maxlength: [50, "Name must be less than 50"]
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-            unique: true
-        },
-        password: {
-            type: String,
-            required: [true, "Password is required"],
-            minlength: [8, "Password must be at least 8 characters"],
-            select: false
-        },
-        role: {
-            type: String,
-            enum: Object.values(AuthRoles),
-            default: AuthRoles.USER
-        },
-        forgotPasswordToken : String,
-        forgotPasswordExpiry: Date,
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      maxlength: [50, "Name must be less than 50"],
     },
-    {
-        timestamps: true
-    }
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
 );
 
-export default mongoose.model("User", userSchema)
+// challenge 1 - encrypt
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export default mongoose.model("User", userSchema);
